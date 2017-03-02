ad01be696a3ef4ecc5c12bc704cfa9555b3e0aee
evost <evost@bitrix24.ru>
2017-03-03 01:08:26 +0300

    Hero rendering

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,14 @@
 #include "io.h"
 #include "Map.h"
+#include "Hero.h"
 
 int main()
 {
 	Init({ 60, 18 }, "Valden");
 	Map VMap(64, 32, 16, 16);
+	Hero VHero(VMap);
 	VMap.GetMap();
+	VHero.GetHero(VMap);
+	VHero.ShowInfo(34, 1);
 	return 0;
 }
