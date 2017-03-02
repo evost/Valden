3e4c8759cb4344a1ed85d3c0c89dbe4b290f81c4
evost <evost@bitrix24.ru>
2017-03-03 00:07:10 +0300

    File with constants

---
+++ constants.h
@@ -0,0 +1,18 @@
+#pragma once
+
+const short Black = 0;
+const short Blue = 1;
+const short Green = 2;
+const short Cyan = 3;
+const short Red = 4;
+const short Magenta = 5;
+const short Brown = 6;
+const short LightGray = 7;
+const short DarkGray = 8;
+const short LightBlue = 9;
+const short LightGreen = 10;
+const short LightCyan = 11;
+const short LightRed = 12;
+const short LightMagenta = 13;
+const short Yellow = 14;
+const short White = 15;
