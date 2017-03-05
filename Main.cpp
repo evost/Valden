94f16e3394e78b69ec0f44c5bf67e3fdaa8235be
evost <evost@bitrix24.ru>
2017-03-06 00:38:39 +0300

    NPCs rendering

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,11 @@
 #include "io.h"
 #include "Map.h"
 #include "Hero.h"
+#include "NPC.h"
 
-void RenderWorld(Map dMap, Hero dHero) {
+void RenderWorld(Map dMap, Hero dHero, NPC dNPC) {
 	dMap.GetMap();
+	dNPC.GetNPCs(dMap);
 	dHero.GetHero(dMap);
 	dHero.ShowInfo(34, 1);
 }
@@ -12,27 +14,29 @@ int main() {
 	Init(60, 18, "Valden");
 	Map VMap(64, 32, 16, 16);
 	Hero VHero(VMap);
-	RenderWorld(VMap, VHero);
+	NPC VNPC(32, VMap);
+	RenderWorld(VMap, VHero, VNPC);
 	while (true) {
 		switch (ReadKey()) {
 		case 224:
 			switch (ReadKey()) {
 			case 72:
-				VHero.HeroStep(0, -1, VMap);
+				if (VNPC.NoNPCs(VHero.x, VHero.y - 1)) VHero.HeroStep(0, -1, VMap);
 				break;
 			case 75:
-				VHero.HeroStep(-1, 0, VMap);
+				if (VNPC.NoNPCs(VHero.x - 1, VHero.y)) VHero.HeroStep(-1, 0, VMap);
 				break;
 			case 77:
-				VHero.HeroStep(1, 0, VMap);
+				if (VNPC.NoNPCs(VHero.x + 1, VHero.y)) VHero.HeroStep(1, 0, VMap);
 				break;
 			case 80:
-				VHero.HeroStep(0, 1, VMap);
+				if (VNPC.NoNPCs(VHero.x, VHero.y + 1)) VHero.HeroStep(0, 1, VMap);
 				break;
 			default:
 				break;
 			}
-			RenderWorld(VMap, VHero);
+			VNPC.NPCstep(VMap, VHero);
+			RenderWorld(VMap, VHero, VNPC);
 			break;
 		case 27:
 			return 0;
