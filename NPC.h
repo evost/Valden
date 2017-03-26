8d3f35ef6bb1cdc627b92c9b5594144e4b0d2478
evost <evost@bitrix24.ru>
2017-03-26 16:47:21 +0300

    NPC.NPCk now public

--- NPC.h
+++ NPC.h
@@ -5,10 +5,10 @@
 
 class NPC {
 private:
-	int NPCk;
 	TNPC NPC_types[2] = { { 0, 3, 2, 1, 5, 3, 1, 3, 0, 0 }, { 1, 5, 3, 3, 7, 5, 2, 4, 0, 0 } };
 	TTile NPC_tiles[2] = { { 0, Brown, '^' }, { 1, Yellow, 'g' } };
 public:
+	int NPCk;
 	TNPC* NPCs = NULL;
 	NPC(int k, Map dMap);
 	void GetNPCs(Map dMap);
