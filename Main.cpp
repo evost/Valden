dfac794df892717727e5e015fe7cccdf23b092f2
evost <evost@bitrix24.ru>
2017-03-26 16:59:23 +0300

    Menu realization in game

--- Main.cpp
+++ Main.cpp
@@ -11,56 +11,23 @@ int main() {
 	Hero VHero(VMap);
 	NPC VNPC(64, VMap);
 	bool showHints = false;
-	short button = 0;
-	while (button != 27) {
-		RenderWorld(VMap, VHero, VNPC, showHints);
-		Render();
-		button = ReadKey();
-		if (VHero.hp > 0)
-			switch (button) {
-			case 224:
-				switch (ReadKey()) {
-				case 72:
-					if (VNPC.NoNPCs(VHero.x, VHero.y - 1)) VHero.HeroStep(0, -1, VMap);
-					break;
-				case 75:
-					if (VNPC.NoNPCs(VHero.x - 1, VHero.y)) VHero.HeroStep(-1, 0, VMap);
-					break;
-				case 77:
-					if (VNPC.NoNPCs(VHero.x + 1, VHero.y)) VHero.HeroStep(1, 0, VMap);
-					break;
-				case 80:
-					if (VNPC.NoNPCs(VHero.x, VHero.y + 1)) VHero.HeroStep(0, 1, VMap);
-					break;
-				default:
-					break;
-				}
-				VNPC.NPCstep(VMap, VHero);
-				break;
-			case 9:
-				showHints = !showHints;
-				break;
-			case 32:
-				VNPC.NPCstep(VMap, VHero);
-				break;
-			default:
-				if (button == 73 || button == 105 || button == 152 || button == 232)
-					VHero.ShowInventory(windowX, windowY);
-				else if (button == 67 || button == 99 || button == 145 || button == 225)
-					VHero.ShowCharacteristics();
-				else {
-					if (button == 87 || button == 119 || button == 150 || button == 230)
-						VHero.ExpInc(VNPC.HeroAttack(VHero.x, VHero.y - 1, VHero.GetDamage()));
-					else if (button == 68 || button == 100 || button == 130 || button == 162)
-						VHero.ExpInc(VNPC.HeroAttack(VHero.x + 1, VHero.y, VHero.GetDamage()));
-					else if (button == 83 || button == 115 || button == 155 || button == 235)
-						VHero.ExpInc(VNPC.HeroAttack(VHero.x, VHero.y + 1, VHero.GetDamage()));
-					else if (button == 65 || button == 97 || button == 148 || button == 228)
-						VHero.ExpInc(VNPC.HeroAttack(VHero.x - 1, VHero.y, VHero.GetDamage()));
-					VNPC.NPCstep(VMap, VHero);
-				}
-				break;
-			}
-	}
-	return 0;
+	bool inGame = false;
+	while (true)
+		switch (Menu(inGame)) {
+		case 1:
+			NewGame(VMap, VHero, VNPC);
+			inGame = true;
+			for (int i = 0; i < logSize; i++)
+				AddLog(sSpace);
+		case 0:
+			Game(VMap, VHero, VNPC, showHints);
+			break;
+		case 2:
+			break;
+		case 3:
+			return 0;
+			break;
+		default:
+			break;
+		}
 }
