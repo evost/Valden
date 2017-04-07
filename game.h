478c205682d8abbc69517dced9cf38ca3b3e4fb0
evost <konstantin.kraynov@yandex.ru>
2017-04-07 17:19:31 +0300

    Removed declaration of the unused functions

--- game.h
+++ game.h
@@ -7,11 +7,6 @@ static wstring gameLog[logSize];
 static short logPosition = 0;
 
 void AddLog(wstring s);
-void ShowGameHints(short x, short y);
-void ShowInventoryHints(short x, short y);
-void ShowCharacteristicsHints(short x, short y);
-void ShowMenuHints(short x, short y);
-void RenderWorld(Map &dMap, Hero &dHero, NPC &dNPC, bool hint);
 void NewHero(Hero &dHero);
 void NewMap(Map &dMap, Hero &dHero, NPC &dNPC);
 void Game(Map &dMap, Hero &dHero, NPC &dNPC, bool &showHints);
